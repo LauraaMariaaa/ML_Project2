370f80fbbadb8797ea6d3311ed735439a301f707
Laura Reimoser <ga87yug@mytum.de>
2021-11-18 10:40:49 +0100

    Tested and debugged preprocessing_combine_brain_amotions.py, technically ready to work now, list of movie names is missing in main

--- preprocessing_combine_brain_emotions.py
+++ preprocessing_combine_brain_emotions.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 
 
-def combine_brain_emotions(movies, emotion, output_path):
+def combine_brain_emotions_per_movie(movies, emotion, output_path):
     ''' This function combines the preprocessed brain and emotion data 
-        in a matrix of format (14*4*l) x (400 + 1 )
-        -> 14 movies, 4 watches per movie, l = length after normalizing over several samplings
+        for one movie in a matrix of format (4*l) x (400 + 1 )
+        -> 4 watches per movie, l = length after normalizing over several samplings
         -> 400 averages of brain regions, 1 column for emotions
         Input: 
-            movies: array of strings with names of movies, preprocessed
-            emotion: string, name of preprocessed emotions file
+            movies: array of strings with names of movies (should be 4 -> 4 views per movie), preprocessed
+            emotion: string, name of preprocessed emotions file, for that specific movie
             output_path: string, name of location where the output should be saved
         Output:
             a .csv file combining the preprocessed emotion and brain data'''
@@ -22,9 +22,9 @@ def combine_brain_emotions(movies, emotion, output_path):
     
     # putting the movie data together
     count = 0
-    for movie in range(movies):
+    for idx, movie in enumerate(movies):
         # load movie data
-        movie_data = np.genfromtxt(movie)
+        movie_data = np.genfromtxt(movie, delimiter=",")
         N = movie_data.shape[0] # number of samples
         D = movie_data.shape[1] # number of features, i.e. brain regions, should be 400
         if N != l: 
@@ -35,15 +35,18 @@ def combine_brain_emotions(movies, emotion, output_path):
             out_movies = movie_data
         # concatenate movies
         else:
-            out_movies = np.concatenate(out_movies, movie_data, axis=0)
+            out_movies = np.concatenate((out_movies, movie_data), axis=0)
 
         count = count + 1
     
     # putting the emotion data together
-    out_emotions = np.tile(l, count)
+ 
+    out_emotions = np.tile(emotion_data, count)
+
 
     # putting everything together
-    out_combined = np.concatenate(out_movies, out_emotions, axis=1)
+
+    out_combined = np.concatenate((out_movies, out_emotions[:, None]), axis=1)
 
     # saving the output
     pd.DataFrame(out_combined).to_csv(output_path,header=None, index=None)
@@ -51,20 +54,68 @@ def combine_brain_emotions(movies, emotion, output_path):
     return 0
 
 
+def combine_brain_emotions(movies, output_path):
+    ''' This function puts together the data (brain and emotion) from all movies in one big matrix
+    Input: 
+        movies: array of strings with names of movies (Combined_...)
+        output_path: string, name of location where the output should be saved
+    Output:
+        a .csv file combining the data for all movies'''
+    
+    count = 0
+    for idx, movie in enumerate(movies):
+        # reading the data
+        movie_data = np.genfromtxt(movie, delimiter=",")
+        # no output data yet (1st iteration)
+        if count == 0:
+            out_movies = movie_data
+        # concatenate movies
+        else:
+            out_movies = np.concatenate((out_movies, movie_data), axis=0)
+        
+        count = count + 1
+    
+    # saving the output
+    pd.DataFrame(out_movies).to_csv(output_path,header=None, index=None)
+
+
+
+
+    return 0
+
+
+
 def main():
+
+    ########################################
+    # TODO: make this for all movies
+    ########################################
 
     # enter all movies in the form results/(movie_name)_averaged.csv
     # List has to be in right order:
-    # movie_1_person_1, movie_1_person_2, ...
-    movies = ["results/BigBuckBunny_TC_400_averaged.csv"]
+    # movie_1_person_1, movie_1_person_2, movie_1_person_4, movie_1_person_4
+    movies_BickBuckBunny = ["datas/BigBuckBunny_TC_400_averaged.csv", 
+                "datas/BigBuckBunny_TC_400_averaged.csv",
+                "datas/BigBuckBunny_TC_400_averaged.csv",
+                "datas/BigBuckBunny_TC_400_averaged.csv"]
 
-    # enter the name of the preprocessed emotion file
-    emotion = "results/emotions.csv"
+    # enter the name of the preprocessed emotion file corresponding to the movie
+    emotion_BickBuckBunny = "datas/Emotions_BigBuckBunny.csv"
 
     # enter the output path
-    output_path = "results/Brain_Emotions_Combined.csv"
+    output_path_BickBuckBunny = "results/Combined_BickBuckBunny.csv"
 
-    combine_brain_emotions(movies, emotion, output_path)
+    combine_brain_emotions_per_movie(movies_BickBuckBunny, emotion_BickBuckBunny, output_path_BickBuckBunny)
+
+    ########################################
+    # TODO: put all output paths names in the list
+    ########################################
+
+    all_movies = [output_path_BickBuckBunny]
+
+    final_output = "results/Combined_All.csv"
+
+    combine_brain_emotions(list, final_output)
 
 
 
